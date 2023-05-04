f484d121f2846bd5655d376ec98923b1287100db
Majid Shajari <majidpy89@gmail.com>
2023-05-04 16:09:18 +0330

    fix issue of event loop

--- stocks.py
+++ stocks.py
@@ -24,31 +24,35 @@ class Stocks:
     def __init__(self, stocks_list: List[str] | str = "all", ):
         self._stocks_list: List[StockDataClass] = []
         self._logger = _logger
-        self._event_loop = asyncio.get_event_loop()
         if stocks_list == "all":
-            self._event_loop.run_until_complete(self._get_all_stocks())
-        self._event_loop.close()
+            self._get_all_stocks()
 
-    async def _get_all_stocks(self):
+    def _get_all_stocks(self):
         """
         """
-        stocks_list = await asyncio.gather(
-            get_stocks_list_from_market_watch_init_page(),
-            get_stocks_list_from_symbols_list_page(),
-            return_exceptions=True)
-        stocks_list = set(stock for result in stocks_list if isinstance(
-            result, list) for stock in result)
-        self._stocks_list = await self._complete_stocks_list(stocks_list)
+        async def _get_stocks():
+            stocks_list = await asyncio.gather(
+                get_stocks_list_from_market_watch_init_page(),
+                get_stocks_list_from_symbols_list_page(),
+                return_exceptions=True)
+            stocks_list = set(stock for result in stocks_list if isinstance(
+                result, list) for stock in result)
+            self._stocks_list = await self._complete_stocks_list(stocks_list)
+
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(_get_stocks())
+        loop.close()
 
     async def _complete_stocks_list(self, stocks_list: List[StockDataClass]):
 
         complete_tasks = [get_stock_with_tse_id(
             stock.current_id) for stock in stocks_list]
-        stocks_list = [result for result in await asyncio.gather(*complete_tasks, return_exceptions=True) if isinstance(result, StockDataClass)]
-        if len(stocks_list) != len(stocks_list):
-            self._logger.info('stocks not complete download, check logger file %s', len(
-                stocks_list)-len(stocks_list))
-        return stocks_list
+        _stocks_list = [result for result in await asyncio.gather(*complete_tasks, return_exceptions=True) if isinstance(result, StockDataClass)]
+        if len(stocks_list) != len(_stocks_list):
+            self._logger.info('stocks not complete download, check logger file : %s Error', len(
+                stocks_list)-len(_stocks_list))
+        return _stocks_list
 
     @property
     def to_dataclass(self) -> List[StockDataClass]:
